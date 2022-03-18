697d6d2c3baf35260b1cea81df39259150ebbe67
melgazar9 <melgazar9@gmail.com>
2022-03-18 17:31:32 -0500

    pulling column names from column transformer

--- dev/scripts/ML_utils.py
+++ dev/scripts/ML_utils.py
@@ -154,8 +154,7 @@ class PreprocessFeatures(TransformerMixin):
                             'custom_features': self.FE_pipeline_dict['custom_pipeline'].values()}
             return feature_dict
 
-        if self.FE_pipeline_dict is not None:
-            if 'custom_pipeline' in self.FE_pipeline_dict.keys():
+        if self.FE_pipeline_dict is not None and 'custom_pipeline' in self.FE_pipeline_dict.keys():
                 custom_features = list(itertools.chain(*self.FE_pipeline_dict['custom_pipeline'].values()))
         else:
             custom_features = []
@@ -281,7 +280,7 @@ class PreprocessFeatures(TransformerMixin):
             hc_pipe = self.FE_pipeline_dict['hc_pipe']
             numeric_pipe = self.FE_pipeline_dict['numeric_pipe']
             oh_pipe = self.FE_pipeline_dict['oh_pipe']
-            custom_pipe = self.FE_pipeline_dict['custom_pipeline']
+            custom_pipe = self.FE_pipeline_dict['custom_pipeline'] if 'custom_pipeline' in self.FE_pipeline_dict.keys() else {}
 
         transformers = [
             ('hc_pipeline', hc_pipe, feature_types['hc_features']),
@@ -311,36 +310,87 @@ class PreprocessFeatures(TransformerMixin):
         return feature_transformer
 
 
-def get_column_names_from_ColumnTransformer(column_transformer):
+def get_column_names_from_ColumnTransformer(column_transformer, clean_column_names=True, verbose=True):
 
-    col_name = []
-    for transformer_in_columns in column_transformer.transformers_[:-1]:  # the last transformer is ColumnTransformer's 'remainder'
-        ds_print(f"\n\ntransformer: {transformer_in_columns[0]}")
-        raw_col_name = list(transformer_in_columns[2])
+    """
+    Reference: Kyle Gilde: https://github.com/kylegilde/Kaggle-Notebooks/blob/master/Extracting-and-Plotting-Scikit-Feature-Names-and-Importances/feature_importance.py
+    Description: Get the column names from the a ColumnTransformer containing transformers & pipelines
+    Parameters
+    ----------
+    verbose: Bool indicating whether to print summaries. Default set to True.
+    Returns
+    -------
+    a list of the correct feature names
+    Note:
+    If the ColumnTransformer contains Pipelines and if one of the transformers in the Pipeline is adding completely new columns,
+    it must come last in the pipeline. For example, OneHotEncoder, MissingIndicator & SimpleImputer(add_indicator=True) add columns
+    to the dataset that didn't exist before, so there should come last in the Pipeline.
+    Inspiration: https://github.com/scikit-learn/scikit-learn/issues/12525
+    """
 
-        if isinstance(transformer_in_columns[1], Pipeline):
-            # if pipeline, get the last transformer
-            transformer = transformer_in_columns[1].steps[-1][1]
-        else:
-            transformer = transformer_in_columns[1]
+    assert isinstance(column_transformer, ColumnTransformer), "Input isn't a ColumnTransformer"
 
-        try:
-            if isinstance(transformer, OneHotEncoder):
-                names = list(transformer.get_feature_names_out(raw_col_name))
+    check_is_fitted(column_transformer)
 
-            elif isinstance(transformer, SimpleImputer) and transformer.add_indicator:
-                missing_indicator_indices = transformer.indicator_.features_
-                missing_indicators = [raw_col_name[idx] + '_missing_flag' for idx in missing_indicator_indices]
-                names = raw_col_name + missing_indicators
+    new_feature_names, transformer_list = [], []
+
+    for i, transformer_item in enumerate(column_transformer.transformers_):
+        transformer_name, transformer, orig_feature_names = transformer_item
+        orig_feature_names = list(orig_feature_names)
+
+        if verbose:
+            print(f"\n\n{i}.Transformer/Pipeline: {transformer_name} {transformer.__class__.__name__}\n")
+            print(f"\tn_orig_feature_names:{len(orig_feature_names)}")
+
+        if transformer == 'drop':
+            continue
+
+        if isinstance(transformer, Pipeline):
+            # if pipeline, get the last transformer in the Pipeline
+            transformer = transformer.steps[-1][1]
+
+        if hasattr(transformer, 'get_feature_names_out'):
+            if 'input_features' in transformer.get_feature_names_out.__code__.co_varnames:
+                names = list(transformer.get_feature_names_out(orig_feature_names))
             else:
                 names = list(transformer.get_feature_names_out())
+        elif hasattr(transformer, 'get_feature_names'):
+            if 'input_features' in transformer.get_feature_names.__code__.co_varnames:
+                names = list(transformer.get_feature_names(orig_feature_names))
+            else:
+                names = list(transformer.get_feature_names())
 
-        except AttributeError as error:
-            names = raw_col_name
+        elif hasattr(transformer, 'indicator_') and transformer.add_indicator:
+            # is this transformer one of the imputers & did it call the MissingIndicator?
 
-        ds_print(names)
-        col_name.extend(names)
-    return col_name
+            missing_indicator_indices = transformer.indicator_.features_
+            missing_indicators = [orig_feature_names[idx] + '_missing_flag' \
+                                  for idx in missing_indicator_indices]
+            names = orig_feature_names + missing_indicators
+
+        elif hasattr(transformer, 'features_'):
+            # is this a MissingIndicator class?
+            missing_indicator_indices = transformer.features_
+            missing_indicators = [orig_feature_names[idx] + '_missing_flag' \
+                                  for idx in missing_indicator_indices]
+
+        else:
+
+            names = orig_feature_names
+
+        if verbose:
+            print(f"\tn_new_features:{len(names)}")
+            print(f"\tnew_features: {names}\n")
+
+        new_feature_names.extend(names)
+        transformer_list.extend([transformer_name] * len(names))
+
+    transformer_list, column_transformer_features = transformer_list, new_feature_names
+
+    if clean_column_names:
+        new_feature_names = list(clean_columns(pd.DataFrame(columns=new_feature_names)).columns)
+
+    return new_feature_names
 
 
 class FeatureImportance:
